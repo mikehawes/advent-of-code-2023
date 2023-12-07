09d1a21264e3ec3c6ff113044a9ee48e2a500c3b
mikehawes <152744602+mikehawes@users.noreply.github.com>
2023-12-07 10:50:48 +0000

    Day 7 part 2 - apply jokers

--- day7/hand.py
+++ day7/hand.py
@@ -1,6 +1,6 @@
 import functools
 
-value_by_card = {
+value_by_card_no_joker = {
     '2': 2,
     '3': 3,
     '4': 4,
@@ -16,19 +16,46 @@ value_by_card = {
     'A': 14
 }
 
+value_by_card_with_joker = {
+    'J': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    'T': 10,
+    'Q': 11,
+    'K': 12,
+    'A': 13
+}
+
 
 @functools.total_ordering
 class Hand:
-    def __init__(self, cards):
+    def __init__(self, cards, value_by_card=None, use_jokers=False):
+        if value_by_card is None:
+            value_by_card = value_by_card_no_joker
         self.cards = cards
+        self.card_values = list(map(lambda c: value_by_card[c], cards))
         self.count_by_card = {}
         for card in cards:
             if card in self.count_by_card:
                 self.count_by_card[card] += 1
             else:
                 self.count_by_card[card] = 1
+        if use_jokers and 'J' in self.count_by_card:
+            num_jokers = self.count_by_card['J']
+            del self.count_by_card['J']
+            if self.count_by_card:
+                most_frequent_card = max(self.count_by_card, key=lambda c: self.count_by_card[c])
+                self.count_by_card[most_frequent_card] += num_jokers
+            else:
+                self.count_by_card['J'] = num_jokers
         counts = self.count_by_card.values()
-        match(max(counts)):
+        match (max(counts)):
             case 1:
                 self.value = 1
             case 2:
@@ -47,6 +74,14 @@ class Hand:
             case 5:
                 self.value = 7
 
+    @staticmethod
+    def from_cards(cards):
+        return Hand(cards, value_by_card_no_joker, False)
+
+    @staticmethod
+    def from_cards_with_joker(cards):
+        return Hand(cards, value_by_card_with_joker, True)
+
     def __eq__(self, other):
         return self.cards == other.cards
 
@@ -56,8 +91,8 @@ class Hand:
         if self.value > other.value:
             return False
         for i in range(0, len(self.cards)):
-            my_card = value_by_card[self.cards[i]]
-            other_card = value_by_card[other.cards[i]]
+            my_card = self.card_values[i]
+            other_card = other.card_values[i]
             if my_card < other_card:
                 return True
             if my_card > other_card:
--- day7/test_hand.py
+++ day7/test_hand.py
@@ -3,50 +3,73 @@ import unittest
 from day7.hand import Hand
 
 
+def hand(cards):
+    return Hand.from_cards(cards)
+
+
+def with_joker(cards):
+    return Hand.from_cards_with_joker(cards)
+
+
 class TestCompareHandByType(unittest.TestCase):
 
     def test_should_find_high_card_is_less_than_one_pair(self):
-        self.assertLess(Hand("23456"), Hand("A23A4"))
+        self.assertLess(hand("23456"), hand("A23A4"))
 
     def test_should_find_one_pair_is_less_than_two_pair(self):
-        self.assertLess(Hand("A23A4"), Hand("23432"))
+        self.assertLess(hand("A23A4"), hand("23432"))
 
     def test_should_find_two_pair_is_less_than_three_of_a_kind(self):
-        self.assertLess(Hand("23432"), Hand("TTT98"))
+        self.assertLess(hand("23432"), hand("TTT98"))
 
     def test_should_find_three_of_a_kind_is_less_than_full_house(self):
-        self.assertLess(Hand("TTT98"), Hand("23332"))
+        self.assertLess(hand("TTT98"), hand("23332"))
 
     def test_should_find_full_house_is_less_than_four_of_a_kind(self):
-        self.assertLess(Hand("23332"), Hand("AA8AA"))
+        self.assertLess(hand("23332"), hand("AA8AA"))
 
     def test_should_find_four_of_a_kind_is_less_than_five_of_a_kind(self):
-        self.assertLess(Hand("AA8AA"), Hand("AAAAA"))
+        self.assertLess(hand("AA8AA"), hand("AAAAA"))
 
 
 class TestCompareHandByCardValue(unittest.TestCase):
 
     def test_should_find_hand_stronger_by_first_card(self):
-        self.assertLess(Hand("2AAAA"), Hand("33332"))
+        self.assertGreater(hand("33332"), hand("2AAAA"))
 
     def test_should_find_hand_stronger_by_third_card(self):
-        self.assertLess(Hand("77788"), Hand("77888"))
+        self.assertGreater(hand("77888"), hand("77788"))
 
     def test_should_find_ten_stronger_than_nine(self):
-        self.assertLess(Hand("92222"), Hand("T2222"))
+        self.assertGreater(hand("T2222"), hand("92222"))
 
     def test_should_find_jack_stronger_than_ten(self):
-        self.assertLess(Hand("T2222"), Hand("J2222"))
+        self.assertGreater(hand("J2222"), hand("T2222"))
 
     def test_should_find_queen_stronger_than_jack(self):
-        self.assertLess(Hand("J2222"), Hand("Q2222"))
+        self.assertGreater(hand("Q2222"), hand("J2222"))
 
     def test_should_find_king_stronger_than_queen(self):
-        self.assertLess(Hand("Q2222"), Hand("K2222"))
+        self.assertGreater(hand("K2222"), hand("Q2222"))
 
     def test_should_find_ace_stronger_than_king(self):
-        self.assertLess(Hand("K2222"), Hand("A2222"))
+        self.assertGreater(hand("A2222"), hand("K2222"))
 
     def test_should_find_first_card_stronger_regardless_of_compare_order(self):
-        self.assertLess(Hand("936A5"), Hand("T75Q2"))
-        self.assertGreater(Hand("T75Q2"), Hand("936A5"))
+        self.assertGreater(hand("T75Q2"), hand("936A5"))
+        self.assertLess(hand("936A5"), hand("T75Q2"))
+
+
+class TestApplyJoker(unittest.TestCase):
+
+    def test_should_find_joker_weaker_for_same_hand(self):
+        self.assertLess(with_joker("J2222"), with_joker("22222"))
+
+    def test_should_find_hand_stronger_when_joker_turns_hand_into_five_of_a_kind(self):
+        self.assertGreater(with_joker("J2222"), with_joker("32222"))
+
+    def test_should_find_five_jokers_stronger_than_four_of_a_kind(self):
+        self.assertGreater(with_joker("JJJJJ"), with_joker("32222"))
+
+    def test_should_find_five_jokers_weaker_than_other_five_of_a_kind(self):
+        self.assertLess(with_joker("JJJJJ"), with_joker("22222"))
