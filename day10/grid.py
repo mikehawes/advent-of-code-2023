807f180de779e3c8bb4b047bc08d499c4a76c8a5
mikehawes <152744602+mikehawes@users.noreply.github.com>
2023-12-10 13:16:50 +0000

    Day 10 part 1 - find left/right connections

--- day10/grid.py
+++ day10/grid.py
@@ -1,3 +1,6 @@
+from itertools import chain
+
+
 class Node:
     def __init__(self, x, y, contents):
         self.x = x
@@ -11,6 +14,8 @@ class Node:
 class Grid:
     def __init__(self, lines):
         self.lines = lines
+        self.width = len(lines[0])
+        self.height = len(lines)
 
     def start_node(self):
         for y, line in enumerate(self.lines):
@@ -21,10 +26,45 @@ class Grid:
     def connected_nodes(self):
         start_node = self.start_node()
         connected_by_location = {start_node.loc_str(): start_node}
+        nodes = [start_node]
+        while len(nodes) > 0:
+            nodes = list(filter(lambda n: n.loc_str() not in connected_by_location,
+                                chain.from_iterable(map(self.next_nodes_of, nodes))))
+            for node in nodes:
+                connected_by_location[node.loc_str()] = node
         return connected_by_location.values()
 
     def nodes_in_path(self):
         return [self.start_node()]
+
+    def next_nodes_of(self, node):
+        return filter(lambda n: n is not None, [
+            self.left_connection_of(node),
+            self.right_connection_of(node)
+        ])
+
+    def left_connection_of(self, node):
+        if node.x == 0:
+            return None
+        if node.contents not in ('-', 'J', '7', 'S'):
+            return None
+        left = self.node_at(node.x - 1, node.y)
+        if left.contents not in ('-', 'F', 'L'):
+            return None
+        return left
+
+    def right_connection_of(self, node):
+        if node.x == self.width - 1:
+            return None
+        if node.contents not in ('-', 'F', 'L', 'S'):
+            return None
+        right = self.node_at(node.x + 1, node.y)
+        if right.contents not in ('-', 'J', '7'):
+            return None
+        return right
+
+    def node_at(self, x, y):
+        return Node(x, y, self.lines[y][x])
 
 
 def read_grid_from_file(input_file):
