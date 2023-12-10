f2bc5b06daf36e750e1f57f1f9b52a0e97dc13c6
mikehawes <152744602+mikehawes@users.noreply.github.com>
2023-12-10 16:59:42 +0000

    Day 10 part 2 - simplify test output

--- day10/grid_printer.py
+++ day10/grid_printer.py
@@ -10,14 +10,17 @@ def listed_node_at(nodes, x, y):
     return None
 
 
-def print_nodes_in_grid(nodes, grid, output):
+def print_outside(grid, path, output):
+    outside_by_loc_str = path.outside_nodes_by_loc_str()
     for y, line in enumerate(grid.lines):
         for x, contents in enumerate(line):
-            path_node = listed_node_at(nodes, x, y)
+            path_node = listed_node_at(path.nodes, x, y)
             if path_node:
                 output.write(path_node.contents)
+            elif to_loc_str(x, y) in outside_by_loc_str:
+                output.write('O')
             else:
-                output.write('.')
+                output.write('*')
         print('', file=output)
     return output.getvalue()
 
@@ -28,22 +31,5 @@ def print_answers(grid):
     print('Furthest position on path:', path.furthest_position(), file=output)
     print('Enclosed tiles:', path.count_enclosed_tiles(), file=output)
     print(file=output)
-    print_nodes_in_grid(path.nodes, grid, output)
-    return output.getvalue()
-
-
-def print_outside(grid):
-    path = Path(grid, grid.start_node())
-    output = io.StringIO()
-    outside_by_loc_str = path.outside_nodes_by_loc_str()
-    for y, line in enumerate(grid.lines):
-        for x, contents in enumerate(line):
-            path_node = listed_node_at(path.nodes, x, y)
-            if path_node:
-                output.write(path_node.contents)
-            elif to_loc_str(x, y) in outside_by_loc_str:
-                output.write('O')
-            else:
-                output.write('.')
-        print('', file=output)
+    print_outside(grid, path, output)
     return output.getvalue()
--- day10/test_grid.py
+++ day10/test_grid.py
@@ -3,7 +3,7 @@ import unittest
 from approvaltests import verify
 
 from day10.grid import read_grid_from_file
-from day10.grid_printer import print_answers, print_outside
+from day10.grid_printer import print_answers
 
 
 class TestGrid(unittest.TestCase):
@@ -27,14 +27,6 @@ class TestGrid(unittest.TestCase):
     def test_should_answer_example5(self):
         grid = read_grid_from_file('example5')
         verify(print_answers(grid))
-
-    def test_should_show_outside_example4(self):
-        grid = read_grid_from_file('example4')
-        verify(print_outside(grid))
-
-    def test_should_show_outside_example5(self):
-        grid = read_grid_from_file('example5')
-        verify(print_outside(grid))
 
     def test_should_answer_input(self):
         grid = read_grid_from_file('input')
