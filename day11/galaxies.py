b01b6b40a2ce8e25d33514d712fe1f8ad6901b9a
mikehawes <152744602+mikehawes@users.noreply.github.com>
2023-12-11 09:20:45 +0000

    Day 11 part 2 - introduce Galaxies class

--- day11/galaxies.py
+++ day11/galaxies.py
@@ -1,33 +1,47 @@
 import re
 
 
-def read_and_expand_space_from_file(input_file):
+class Galaxies:
+    def __init__(self, lines):
+        self.lines = lines
+        self.expansion_by_y = {}
+        for y, line in enumerate(lines):
+            if re.match(r'^\.+$', line):
+                self.expansion_by_y[y] = True
+        self.expansion_by_x = {}
+        for x in range(0, len(lines[0])):
+            col_empty = True
+            for y in range(0, len(lines)):
+                if lines[y][x] != '.':
+                    col_empty = False
+                    break
+            if col_empty:
+                self.expansion_by_x[x] = True
+
+    def expand_lines(self):
+        expanded_lines = []
+        for y, line in enumerate(self.lines):
+            expanded_line = []
+            for x, contents in enumerate(line):
+                expanded_line.append(contents)
+                if x in self.expansion_by_x:
+                    expanded_line.append(contents)
+            expanded_line = ''.join(expanded_line)
+            expanded_lines.append(expanded_line)
+            if y in self.expansion_by_y:
+                expanded_lines.append(expanded_line)
+        return expanded_lines
+
+
+def read_galaxies_from_file(input_file):
     with open(input_file, 'r') as file:
         raw_lines = file.readlines()
-    lines = []
-    for i, raw_line in enumerate(raw_lines):
-        line = raw_line.strip()
-        if re.match(r'^\.+$', line):
-            lines.append(line)
-        lines.append(line)
-    expanded_lines = lines.copy()
-    col_expansions = 0
-    for x in range(0, len(lines[0])):
-        col_empty = True
-        for y in range(0, len(lines)):
-            if lines[y][x] != '.':
-                col_empty = False
-                break
-        if col_empty:
-            for y in range(0, len(expanded_lines)):
-                line = expanded_lines[y]
-                pos = x + col_expansions
-                if pos < len(line) - 1:
-                    expanded_lines[y] = line[:pos] + line[pos] + line[pos:]
-                else:
-                    expanded_lines[y] = line + line[pos]
-            col_expansions += 1
-    return expanded_lines
+    lines = list(map(lambda ln: ln.strip(), raw_lines))
+    return Galaxies(lines)
+
+
+def read_and_expand_space_from_file(input_file):
+    return read_galaxies_from_file(input_file).expand_lines()
 
 
 def sum_galaxy_distances_from_file(input_file):
