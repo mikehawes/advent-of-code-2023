637670d9346c4e7cbc0b955cdc28af867ad28b1c
mikehawes <152744602+mikehawes@users.noreply.github.com>
2023-12-11 08:57:36 +0000

    Day 11 part 1

--- day11/galaxies.py
+++ day11/galaxies.py
@@ -28,3 +28,19 @@ def read_and_expand_space_from_file(input_file):
                     expanded_lines[y] = line + line[pos]
             col_expansions += 1
     return expanded_lines
+
+
+def sum_galaxy_distances_from_file(input_file):
+    lines = read_and_expand_space_from_file(input_file)
+    galaxies = []
+    for y, line in enumerate(lines):
+        for x, contents in enumerate(line):
+            if contents == '#':
+                galaxies.append((x, y))
+    total_distance = 0
+    for i, a in enumerate(galaxies[:len(galaxies) - 1]):
+        for b in galaxies[i + 1:]:
+            dist_x = abs(b[0] - a[0])
+            dist_y = abs(b[1] - a[1])
+            total_distance += dist_x + dist_y
+    return total_distance
--- day11/test_galaxies.py
+++ day11/test_galaxies.py
@@ -2,10 +2,16 @@ import unittest
 
 from approvaltests import verify
 
-from day11.galaxies import read_and_expand_space_from_file
+from day11.galaxies import read_and_expand_space_from_file, sum_galaxy_distances_from_file
 
 
 class TestGalaxies(unittest.TestCase):
 
     def test_should_expand_empty_space_in_example(self):
         verify('\n'.join(read_and_expand_space_from_file('example')))
+
+    def test_should_sum_distances_in_example(self):
+        self.assertEqual(374, sum_galaxy_distances_from_file('example'))
+
+    def test_should_sum_distances_in_input(self):
+        self.assertEqual(9684228, sum_galaxy_distances_from_file('input'))
