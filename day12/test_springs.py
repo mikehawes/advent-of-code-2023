c4017881f4a59f979efb189aac9ae7d3784abe25
mikehawes <152744602+mikehawes@users.noreply.github.com>
2023-12-12 21:54:34 +0000

    Day 12 part 2 - fix test class name

--- day12/test_springs.py
+++ day12/test_springs.py
@@ -7,7 +7,7 @@ from day12.springs import SpringConditionRecord, total_spring_arrangements_from_
 from day12.springs_printer import print_working_spring_arrangements_for_file
 
 
-class TestGalaxies(unittest.TestCase):
+class TestSprings(unittest.TestCase):
 
     def test_should_print_working_spring_arrangements_for_example(self):
         verify(print_working_spring_arrangements_for_file('example'))
