956cb0820a0a243c65d88e6941ebf668d35d3d83
mikehawes <152744602+mikehawes@users.noreply.github.com>
2023-12-12 16:34:49 +0000

    Day 12 part 1 - generate arrangements

--- day12/springs.py
+++ day12/springs.py
@@ -8,7 +8,35 @@ class Area:
         self.end = match.end()
         self.length = self.end - self.start
         self.contents = match.group(0)
-        self.known = self.contents.startswith('#')
+        self.type = self.contents[0]
+        self.known = self.type != '?'
+
+
+def generate_all_fillings(areas, area_index=0, area_offset=0, filling_start=''):
+    if area_index >= len(areas):
+        return [filling_start]
+    area = areas[area_index]
+    if area.known:
+        return generate_all_fillings(areas, area_index + 1, 0, filling_start + area.contents)
+    if area_offset >= area.length:
+        return generate_all_fillings(areas, area_index + 1, 0, filling_start)
+    fillings_with_damaged = generate_all_fillings(areas, area_index, area_offset + 1, filling_start + '#')
+    fillings_with_undamaged = generate_all_fillings(areas, area_index, area_offset + 1, filling_start + '.')
+    return fillings_with_damaged + fillings_with_undamaged
+
+
+def find_damaged_counts(springs):
+    counts = []
+    num_damaged = 0
+    for spring in springs:
+        if spring == '#':
+            num_damaged += 1
+        elif num_damaged > 0:
+            counts.append(num_damaged)
+            num_damaged = 0
+    if num_damaged > 0:
+        counts.append(num_damaged)
+    return counts
 
 
 class DamagedArea:
@@ -24,24 +52,41 @@ class DamagedArea:
         self.fully_known = len(self.known_damaged) == 1 and self.length == self.known_damaged[0].length
         self.fully_unknown = len(self.unknown_areas) == 1 and self.length == self.unknown_areas[0].length
 
-    def generate_all_fillings(self, area_index=0, area_offset=0, filling_start=''):
-        if area_index >= len(self.areas):
-            return [filling_start]
-        area = self.areas[area_index]
-        if area.known:
-            return self.generate_all_fillings(area_index + 1, 0, filling_start + area.contents)
-        if area_offset >= area.length:
-            return self.generate_all_fillings(area_index + 1, 0, filling_start)
-        fillings_with_damaged = self.generate_all_fillings(area_index, area_offset + 1, filling_start + '#')
-        fillings_with_undamaged = self.generate_all_fillings(area_index, area_offset + 1, filling_start + '.')
-        return fillings_with_damaged + fillings_with_undamaged
+    def generate_all_fillings(self):
+        return generate_all_fillings(self.areas)
+
+
+class SpringArrangements:
+    def __init__(self, record, fillings, arrangements):
+        self.record = record
+        self.fillings = fillings
+        self.arrangements = arrangements
 
 
 class SpringConditionRecord:
     def __init__(self, springs, damaged_counts):
         self.springs = springs
         self.damaged_counts = damaged_counts
+        self.areas = list(map(Area, re.finditer(r'#+|\.+|\?+', springs)))
         self.damaged_areas = list(map(DamagedArea, re.finditer(r'[?#]+', springs)))
+
+    def generate_all_fillings(self):
+        return generate_all_fillings(self.areas)
+
+    def possible_fillings(self, fillings=None):
+        if not fillings:
+            fillings = self.generate_all_fillings()
+        possible = []
+        for filling in fillings:
+            damaged_counts = find_damaged_counts(filling)
+            if damaged_counts == self.damaged_counts:
+                possible.append(filling)
+        return possible
+
+    def arrangements(self):
+        fillings = self.generate_all_fillings()
+        arrangements = self.possible_fillings(fillings)
+        return SpringArrangements(self, fillings, arrangements)
 
 
 def read_spring_condition_line(line):
--- day12/springs_printer.py
+++ day12/springs_printer.py
@@ -5,11 +5,21 @@ from day12.springs import read_spring_conditions_from_file
 
 def print_working_spring_arrangements_for_file(input_file):
     records = read_spring_conditions_from_file(input_file)
+    record_arrangements = list(map(lambda r: r.arrangements(), records))
+    total_arrangements = sum(map(lambda a: len(a.arrangements), record_arrangements))
     output = io.StringIO()
-    for record in records:
+    print('Total arrangements:', total_arrangements, file=output)
+    print(file=output)
+    for arrangements in record_arrangements:
+        record = arrangements.record
         print('{} {}'.format(record.springs, ','.join(map(str, record.damaged_counts))), file=output)
+        num_fillings = len(arrangements.fillings)
+        num_arrangements = len(arrangements.arrangements)
+        print('{} filling{}'.format(num_fillings, '' if num_fillings == 1 else 's'), file=output)
+        print('{} arrangement{}:'.format(num_arrangements, '' if num_arrangements == 1 else 's'), file=output)
+        for arrangement in arrangements.arrangements:
+            print(arrangement, file=output)
         damaged_areas = len(record.damaged_areas)
-        print('{} arrangements'.format('?'), file=output)
         print('{} damaged area{}:'.format(damaged_areas, '' if damaged_areas == 1 else 's'), file=output)
         for area in record.damaged_areas:
             output.write('{}-{}: {} - length {}'.format(area.start, area.end, area.contents, area.length))
@@ -24,8 +34,6 @@ def print_working_spring_arrangements_for_file(input_file):
                     output.write(', {} unknown area{}'.format(unknown_areas, '' if unknown_areas == 1 else 's'))
                 if known_areas > 0:
                     output.write(', {} known area{}'.format(known_areas, '' if known_areas == 1 else 's'))
-                fillings = area.generate_all_fillings()
-                output.write(', {} fillings'.format(len(fillings)))
                 print(file=output)
         print(file=output)
     return output.getvalue()
