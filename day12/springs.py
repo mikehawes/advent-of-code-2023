0e89488507e1e9039ee79a06593d6688a30e6f6b
mikehawes <152744602+mikehawes@users.noreply.github.com>
2023-12-13 20:20:57 +0000

    Day 12 part 2 - move arrangements generation to its own file

--- day12/springs.py
+++ day12/springs.py
@@ -2,7 +2,8 @@ import datetime
 import itertools
 import re
 import time
-from math import prod
+
+from day12.arrangements import generate_arrangements
 
 
 class Area:
@@ -16,105 +17,6 @@ class Area:
 
 def area_from_match(match):
     return Area(match.group(0))
-
-
-class FindArrangementsState:
-    def __init__(self, damaged_counts=None, remaining_unknown=None, remaining_unknown_damaged=None,
-                 damaged_count=0, force_undamaged=False, area_index=0, area_offset=0, valid=True):
-        if not valid or remaining_unknown < remaining_unknown_damaged:
-            self.valid = False
-            return
-        self.damaged_counts = damaged_counts
-        self.remaining_unknown = remaining_unknown
-        self.remaining_unknown_damaged = remaining_unknown_damaged
-        self.damaged_count = damaged_count
-        self.force_undamaged = force_undamaged
-        self.area_index = area_index
-        self.area_offset = area_offset
-        self.valid = True
-        if not damaged_counts:
-            self.force_undamaged = True
-        elif damaged_count > damaged_counts[0]:
-            self.valid = False
-        elif damaged_count == damaged_counts[0]:
-            self.damaged_counts = damaged_counts[1:]
-            self.damaged_count = 0
-            self.force_undamaged = True
-
-    def after_known_area(self, area):
-        if self.force_undamaged and area.damaged:
-            return FindArrangementsState(valid=False)
-        if self.damaged_count > 0 and not area.damaged:
-            return FindArrangementsState(valid=False)
-        found_damaged = area.length if area.damaged else 0
-        return FindArrangementsState(self.damaged_counts, self.remaining_unknown, self.remaining_unknown_damaged,
-                                     self.damaged_count + found_damaged, False, self.area_index + 1, 0)
-
-    def next_area(self):
-        return FindArrangementsState(self.damaged_counts, self.remaining_unknown, self.remaining_unknown_damaged,
-                                     self.damaged_count, self.force_undamaged, self.area_index + 1, 0)
-
-    def chose_unknown_undamaged(self):
-        return FindArrangementsState(self.damaged_counts, self.remaining_unknown - 1, self.remaining_unknown_damaged,
-                                     0, False, self.area_index, self.area_offset + 1)
-
-    def chose_unknown_damaged(self):
-        return FindArrangementsState(self.damaged_counts, self.remaining_unknown - 1,
-                                     self.remaining_unknown_damaged - 1,
-                                     self.damaged_count + 1, False, self.area_index, self.area_offset + 1)
-
-
-def generate_arrangements(areas, damaged_counts, count_only):
-    unknown = sum(map(lambda a: a.length, filter(lambda a: not a.known, areas)))
-    known_damaged = sum(map(lambda a: a.length, filter(lambda a: a.damaged, areas)))
-    unknown_damaged = sum(damaged_counts) - known_damaged
-    state = FindArrangementsState(damaged_counts, unknown, unknown_damaged)
-    state_stack = []
-    filling_stack = []
-    filling_start = ''
-    arrangements = 0 if count_only else []
-    while True:
-        if not state.valid:
-            if state_stack:
-                state = state_stack.pop()
-                if not count_only:
-                    filling_start = filling_stack.pop()
-            else:
-                return arrangements
-            continue
-        if state.area_index >= len(areas):
-            if len(state.damaged_counts) == 0:
-                if count_only:
-                    arrangements += 1
-                else:
-                    arrangements.append(filling_start)
-            if state_stack:
-                state = state_stack.pop()
-                if not count_only:
-                    filling_start = filling_stack.pop()
-            else:
-                return arrangements
-            continue
-        area = areas[state.area_index]
-        if area.known:
-            state = state.after_known_area(area)
-            if not count_only:
-                filling_start += area.contents
-            continue
-        if state.area_offset >= area.length:
-            state = state.next_area()
-            continue
-        if state.damaged_count == 0:
-            state_stack.append(state.chose_unknown_undamaged())
-            if not count_only:
-                filling_stack.append(filling_start + '.')
-        if not state.force_undamaged:
-            state_stack.append(state.chose_unknown_damaged())
-            if not count_only:
-                filling_stack.append(filling_start + '#')
-        state = state_stack.pop()
-        if not count_only:
-            filling_start = filling_stack.pop()
 
 
 class DamagedArea:
@@ -131,17 +33,6 @@ class DamagedArea:
         self.fully_unknown = len(self.unknown_areas) == 1 and self.length == self.unknown_areas[0].length
 
 
-class SpringArrangements:
-    def __init__(self, record, fillings_count, arrangements):
-        self.record = record
-        self.fillings_count = fillings_count
-        if isinstance(arrangements, list):
-            self.arrangements = arrangements
-            self.arrangements_count = len(arrangements)
-        else:
-            self.arrangements_count = arrangements
-
-
 class SpringConditionRecord:
     def __init__(self, springs, damaged_counts, unfolded_from=None):
         self.springs = springs
@@ -151,9 +42,7 @@ class SpringConditionRecord:
         self.damaged_areas = list(map(DamagedArea, re.finditer(r'[?#]+', springs)))
 
     def arrangements(self, count_only=False):
-        fillings_count = prod(map(lambda a: 1 if a.known else 2 ** a.length, self.areas))
-        arrangements = generate_arrangements(self.areas, self.damaged_counts, count_only)
-        return SpringArrangements(self, fillings_count, arrangements)
+        return generate_arrangements(self, count_only)
 
     def count_arrangements(self, multiple=1):
         return self.unfold(multiple).arrangements(count_only=True).arrangements_count
