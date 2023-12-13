5b8d18f3be28fb79000c4fb2bed7b394fef44d3f
mikehawes <152744602+mikehawes@users.noreply.github.com>
2023-12-13 10:26:27 +0000

    Day 12 part 2 - more tests

--- day12/springs_printer.py
+++ day12/springs_printer.py
@@ -1,10 +1,28 @@
 import io
 
-from day12.springs import compute_spring_arrangements_from_file, total_spring_arrangements
+from day12.springs import total_spring_arrangements, \
+    read_spring_conditions_from_file, compute_spring_arrangements_from_records
+
+
+def print_damaged_counts(record):
+    return ','.join(map(str, record.damaged_counts))
+
+
+def print_record(record):
+    return '{} {}'.format(record.springs, print_damaged_counts(record))
+
+
+def list_of_record_and_arrangement_count(records, multiple=1):
+    return list(map(lambda r: [print_record(r), r.count_arrangements(multiple=multiple)], records))
 
 
 def print_working_spring_arrangements_for_file(input_file, multiple=1):
-    record_arrangements = compute_spring_arrangements_from_file(input_file, multiple=multiple)
+    records = read_spring_conditions_from_file(input_file)
+    return print_working_spring_arrangements_for_records(records, multiple)
+
+
+def print_working_spring_arrangements_for_records(records, multiple=1):
+    record_arrangements = compute_spring_arrangements_from_records(records, multiple=multiple)
     total_arrangements = total_spring_arrangements(record_arrangements)
     output = io.StringIO()
     print('Total arrangements:', total_arrangements, file=output)
@@ -13,7 +31,7 @@ def print_working_spring_arrangements_for_file(input_file, multiple=1):
     print(file=output)
     for arrangements in record_arrangements:
         record = arrangements.record
-        print('{} {}'.format(record.springs, ','.join(map(str, record.damaged_counts))), file=output)
+        print('{} {}'.format(record.springs, print_damaged_counts(record)), file=output)
         num_fillings = arrangements.fillings_count
         arrangements_count = arrangements.arrangements_count
         print('{} filling{}'.format(num_fillings, '' if num_fillings == 1 else 's'), file=output)
--- day12/test_springs.py
+++ day12/test_springs.py
@@ -4,7 +4,8 @@ from approvaltests import verify
 
 from day12.springs import SpringConditionRecord, total_spring_arrangements_from_file, read_spring_conditions_from_file, \
     total_spring_arrangements_from_records
-from day12.springs_printer import print_working_spring_arrangements_for_file
+from day12.springs_printer import print_working_spring_arrangements_for_file, list_of_record_and_arrangement_count, \
+    print_working_spring_arrangements_for_records
 
 
 class TestSprings(unittest.TestCase):
@@ -54,35 +55,65 @@ class TestSprings(unittest.TestCase):
         record = SpringConditionRecord("??#?#?????????????.", [8, 4, 1])
         self.assertEqual(67_192_396, record.count_arrangements(multiple=5))
 
+
+class TestSpringsInputLine41(unittest.TestCase):
+    def setUp(self):
+        self.records = [
+            SpringConditionRecord("????#????.???", [1, 2, 1, 1]),
+            SpringConditionRecord("????#????", [1, 2, 1, 1]),
+            SpringConditionRecord("????#????", [1, 2, 1]),
+            SpringConditionRecord("????#????", [1, 2]),
+            SpringConditionRecord("???", [1]),
+            SpringConditionRecord("???", [1, 1])
+        ]
+        self.partial_records = self.records[1:]
+        self.record = self.records[0]
+
     @unittest.skip('Too slow')
-    def test_should_multiply_arrangements_input_line_41(self):
-        record = SpringConditionRecord("????#????.???", [1, 2, 1, 1])
-        self.assertEqual(0, record.count_arrangements(multiple=5))
+    def test_should_find_arrangements_for_multiple_5(self):
+        self.assertEqual(0, self.record.count_arrangements(multiple=5))
 
-    def test_should_multiply_arrangements_input_line_41_a(self):
-        record = SpringConditionRecord("????#????", [1, 2, 1, 1])
-        self.assertEqual(3880, record.count_arrangements(multiple=5))
+    def test_should_find_arrangements_for_partial_multiple_5(self):
+        self.assertEqual([
+            ['????#???? 1,2,1,1', 3880],
+            ['????#???? 1,2,1', 3381560],
+            ['????#???? 1,2', 366283],
+            ['??? 1', 3003],
+            ['??? 1,1', 1]],
+            list_of_record_and_arrangement_count(self.partial_records, multiple=5))
 
-    def test_should_multiply_arrangements_input_line_41_b(self):
-        record = SpringConditionRecord("????#????", [1, 2])
-        self.assertEqual(366283, record.count_arrangements(multiple=5))
+    def test_should_find_arrangements_for_multiple_4(self):
+        self.assertEqual([
+            ['????#????.??? 1,2,1,1', 50316280],
+            ['????#???? 1,2,1,1', 542],
+            ['????#???? 1,2,1', 140574],
+            ['????#???? 1,2', 22933],
+            ['??? 1', 495],
+            ['??? 1,1', 1]],
+            list_of_record_and_arrangement_count(self.records, multiple=4))
 
-    def test_should_multiply_arrangements_input_line_41_with_lower_multiple(self):
-        record = SpringConditionRecord("????#????.???", [1, 2, 1, 1])
-        self.assertEqual(50_316_280, record.count_arrangements(multiple=4))
+    def test_should_find_arrangements(self):
+        self.assertEqual([
+            ['????#????.??? 1,2,1,1', 45],
+            ['????#???? 1,2,1,1', 2],
+            ['????#???? 1,2,1', 12],
+            ['????#???? 1,2', 7],
+            ['??? 1', 3],
+            ['??? 1,1', 1]],
+            list_of_record_and_arrangement_count(self.records))
 
-    def test_should_multiply_arrangements_input_line_41_with_lower_multiple_a(self):
-        record = SpringConditionRecord("????#????", [1, 2, 1, 1])
-        self.assertEqual(542, record.count_arrangements(multiple=4))
+    def test_should_find_arrangements_for_multiple_2(self):
+        self.assertEqual([
+            ['????#????.??? 1,2,1,1', 4414],
+            ['????#???? 1,2,1,1', 11],
+            ['????#???? 1,2,1', 256],
+            ['????#???? 1,2', 97],
+            ['??? 1', 15],
+            ['??? 1,1', 1]],
+            list_of_record_and_arrangement_count(self.records, multiple=2))
 
-    def test_should_multiply_arrangements_input_line_41_with_lower_multiple_b(self):
-        record = SpringConditionRecord("????#????", [1, 2, 1])
-        self.assertEqual(140574, record.count_arrangements(multiple=4))
+    def test_should_print_arrangements(self):
+        verify(print_working_spring_arrangements_for_records(self.records))
 
-    def test_should_multiply_arrangements_input_line_41_with_lower_multiple_c(self):
-        record = SpringConditionRecord("????#????", [1, 2])
-        self.assertEqual(22933, record.count_arrangements(multiple=4))
-
-    def test_should_multiply_arrangements_input_line_41_with_lower_multiple_d(self):
-        record = SpringConditionRecord("???", [1])
-        self.assertEqual(495, record.count_arrangements(multiple=4))
+    def test_should_print_arrangements_with_multiple_2(self):
+        verify(print_working_spring_arrangements_for_records(self.records, multiple=2))
