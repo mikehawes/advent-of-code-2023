c2ad4898d0b679d3278b2b269d5ef7800286c253
mikehawes <152744602+mikehawes@users.noreply.github.com>
2023-12-13 22:42:47 +0000

    Day 13 part 2

--- day13/reflection.py
+++ day13/reflection.py
@@ -8,23 +8,28 @@ def load_patterns_from_file(input_file):
                 patterns.append(pattern_lines)
                 pattern_lines = []
             else:
-                pattern_lines.append(line)
+                pattern_lines.append(list(line))
         patterns.append(pattern_lines)
         return patterns
 
 
+class ReflectionLine:
+    def __init__(self, is_horizontal, offset):
+        self.is_horizontal = is_horizontal
+        self.offset = offset
+
+
 class ReflectionLines:
-    def __init__(self, pattern_lines, x_values, y_values):
-        self.pattern_lines = pattern_lines
-        self.x_values = x_values
-        self.y_values = y_values
+    def __init__(self, lines):
+        self.lines = lines
 
     def score(self):
         total = 0
-        for x in self.x_values:
-            total += x + 1
-        for y in self.y_values:
-            total += 100 * (y + 1)
+        for line in self.lines:
+            if line.is_horizontal:
+                total += line.offset + 1
+            else:
+                total += 100 * (line.offset + 1)
         return total
 
 
@@ -33,8 +38,6 @@ def find_reflection_lines(pattern_lines):
     width = len(pattern_lines[0])
     x_values = []
     y_values = []
-    for line in pattern_lines:
-        print(line, flush=True)
     for y in range(0, height - 1):
         is_reflection = True
         for i in range(0, min(y + 1, height - y - 1)):
@@ -42,7 +45,6 @@ def find_reflection_lines(pattern_lines):
                 is_reflection = False
         if is_reflection:
             y_values.append(y)
-            print('Found reflection at y={}'.format(y), flush=True)
     for x in range(0, width - 1):
         is_reflection = True
         for i in range(0, min(x + 1, width - x - 1)):
@@ -51,16 +53,83 @@ def find_reflection_lines(pattern_lines):
                     is_reflection = False
         if is_reflection:
             x_values.append(x)
-            print('Found reflection at x={}'.format(x), flush=True)
-    return ReflectionLines(pattern_lines, x_values, y_values)
+
+    lines = []
+    for value in x_values:
+        lines.append(ReflectionLine(True, value))
+    for value in y_values:
+        lines.append(ReflectionLine(False, value))
+    return ReflectionLines(lines)
+
+
+def unsmudge_and_find_new_lines(pattern_lines, reflection_lines=None):
+    height = len(pattern_lines)
+    width = len(pattern_lines[0])
+    unsmudged = pattern_lines.copy()
+    if not reflection_lines:
+        reflection_lines = find_reflection_lines(pattern_lines)
+    line_smudged = reflection_lines.lines[0]
+    for x in range(0, width):
+        for y in range(0, height):
+            line = unsmudged[y]
+            old_char = line[x]
+            if old_char == '.':
+                line[x] = '#'
+            else:
+                line[x] = '.'
+            found_unsmudged = find_reflection_lines(unsmudged)
+            new_lines = []
+            if found_unsmudged.lines:
+                for found_line in found_unsmudged.lines:
+                    if found_line.is_horizontal != line_smudged.is_horizontal or found_line.offset != line_smudged.offset:
+                        new_lines.append(found_line)
+            if new_lines:
+                return unsmudged, ReflectionLines(new_lines)
+            line[x] = old_char
+    return pattern_lines, None
+
+
+def print_pattern_lines(pattern_lines):
+    for line in pattern_lines:
+        print(''.join(line), flush=True)
+
+
+def print_reflection_lines(reflections):
+    for line in reflections.lines:
+        if line.is_horizontal:
+            print('Found reflection at x={}'.format(line.offset), flush=True)
+        else:
+            print('Found reflection at y={}'.format(line.offset), flush=True)
 
 
 def compute_reflections_number_from_file(input_file):
     patterns = load_patterns_from_file(input_file)
 
     total = 0
+    print()
     for pattern_lines in patterns:
+        print_pattern_lines(pattern_lines)
         reflection_lines = find_reflection_lines(pattern_lines)
+        print_reflection_lines(reflection_lines)
         total += reflection_lines.score()
+        print()
+
+    return total
+
+
+def unsmudge_and_compute_reflections_number_from_file(input_file):
+    patterns = load_patterns_from_file(input_file)
+
+    total = 0
+    print()
+    for pattern_lines in patterns:
+        print_pattern_lines(pattern_lines)
+        reflection_lines = find_reflection_lines(pattern_lines)
+        print_reflection_lines(reflection_lines)
+        unsmudged, unsmudged_lines = unsmudge_and_find_new_lines(pattern_lines, reflection_lines)
+        print_pattern_lines(unsmudged)
+        print_reflection_lines(unsmudged_lines)
+        total += unsmudged_lines.score()
+        print()
 
     return total
--- day13/test_reflection.py
+++ day13/test_reflection.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day13.reflection import compute_reflections_number_from_file
+from day13.reflection import compute_reflections_number_from_file, unsmudge_and_compute_reflections_number_from_file
 
 
 class TestReflections(unittest.TestCase):
@@ -10,3 +10,9 @@ class TestReflections(unittest.TestCase):
 
     def test_should_answer_input(self):
         self.assertEqual(30158, compute_reflections_number_from_file('input'))
+
+    def test_should_unsmudge_and_answer_example(self):
+        self.assertEqual(400, unsmudge_and_compute_reflections_number_from_file('example'))
+
+    def test_should_unsmudge_and_answer_input(self):
+        self.assertEqual(36474, unsmudge_and_compute_reflections_number_from_file('input'))
