4853b45e9d0c34ce560c2ab03614b00db4443305
mikehawes <152744602+mikehawes@users.noreply.github.com>
2023-12-15 22:48:22 +0000

    Day 15 part 2 - print boxes after each step

--- day15/hash.py
+++ day15/hash.py
@@ -1,3 +1,6 @@
+import io
+
+
 def hash_string(string):
     value = 0
     for char in string:
@@ -11,3 +14,52 @@ def sum_hashes_for_file(input_file):
     with open(input_file, 'r') as file:
         steps = file.readline().strip().split(',')
     return sum(map(hash_string, steps))
+
+
+class Lens:
+    def __init__(self, label, focal_length):
+        self.label = label
+        self.focal_length = focal_length
+
+
+def print_boxes(boxes, out):
+    for i, box in enumerate(boxes):
+        if box:
+            printed_lenses = list(map(lambda lens: '{} {}'.format(lens.label, lens.focal_length), box))
+            print('Box {}: {}'.format(i, printed_lenses), file=out)
+
+
+def find_position_with_label(box, label):
+    return next(map(lambda p: p[0],
+                    filter(lambda p: p[1].label == label,
+                           enumerate(box))),
+                None)
+
+
+def print_steps_for_file(input_file):
+    with open(input_file, 'r') as file:
+        steps = file.readline().strip().split(',')
+    out = io.StringIO()
+    boxes = list(map(lambda n: [], range(0, 256)))
+    for step in steps:
+        if step.endswith('-'):
+            label = step[:-1]
+            box = boxes[hash_string(label)]
+            position = find_position_with_label(box, label)
+            if position is not None:
+                del box[position]
+        else:
+            parts = step.split('=')
+            label = parts[0]
+            focal_length = parts[1]
+            box = boxes[hash_string(label)]
+            position = find_position_with_label(box, label)
+            lens = Lens(label, focal_length)
+            if position is not None:
+                box[position] = lens
+            else:
+                box.append(lens)
+
+        print('After "{}":'.format(step), file=out)
+        print_boxes(boxes, out)
+    return out.getvalue()
--- day15/test_hash.py
+++ day15/test_hash.py
@@ -1,6 +1,8 @@
 import unittest
 
-from day15.hash import hash_string, sum_hashes_for_file
+from approvaltests import verify
+
+from day15.hash import hash_string, sum_hashes_for_file, print_steps_for_file
 
 
 class TestHash(unittest.TestCase):
@@ -8,8 +10,11 @@ class TestHash(unittest.TestCase):
     def test_should_hash_HASH(self):
         self.assertEqual(52, hash_string('HASH'))
 
-    def test_should_sum_hashes_for_example(self):
+    def test_should_sum_step_hashes_for_example(self):
         self.assertEqual(1320, sum_hashes_for_file('example'))
 
-    def test_should_sum_hashes_for_input(self):
+    def test_should_sum_step_hashes_for_input(self):
         self.assertEqual(505427, sum_hashes_for_file('input'))
+
+    def test_should_print_steps_for_example(self):
+        verify(print_steps_for_file('example'))
