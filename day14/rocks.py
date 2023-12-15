71dcf2cf53b87f9d437f1d5dcda29bd3c0b489e1
mikehawes <152744602+mikehawes@users.noreply.github.com>
2023-12-15 20:19:52 +0000

    Day 14 part 2

--- day14/rocks.py
+++ day14/rocks.py
@@ -1,4 +1,5 @@
 import io
+from copy import deepcopy
 
 
 class PlatformState:
@@ -8,7 +9,7 @@ class PlatformState:
         self.width = len(self.lines[0])
 
     def tilt_north(self):
-        new_lines = self.lines.copy()
+        new_lines = deepcopy(self.lines)
         for y in range(0, self.height):
             for x in range(0, self.width):
                 if new_lines[y][x] == '.':
@@ -23,7 +24,7 @@ class PlatformState:
         return PlatformState(new_lines)
 
     def tilt_west(self):
-        new_lines = self.lines.copy()
+        new_lines = deepcopy(self.lines)
         for x in range(0, self.width):
             for y in range(0, self.height):
                 if new_lines[y][x] == '.':
@@ -38,7 +39,7 @@ class PlatformState:
         return PlatformState(new_lines)
 
     def tilt_east(self):
-        new_lines = self.lines.copy()
+        new_lines = deepcopy(self.lines)
         for x in range(self.width - 1, -1, -1):
             for y in range(0, self.height):
                 if new_lines[y][x] == '.':
@@ -53,7 +54,7 @@ class PlatformState:
         return PlatformState(new_lines)
 
     def tilt_south(self):
-        new_lines = self.lines.copy()
+        new_lines = deepcopy(self.lines)
         for y in range(self.height - 1, -1, -1):
             for x in range(0, self.width):
                 if new_lines[y][x] == '.':
@@ -70,13 +71,51 @@ class PlatformState:
     def spin_cycle(self):
         return self.tilt_north().tilt_west().tilt_south().tilt_east()
 
+    def spin_cycles(self, times):
+        state = self
+        for i in range(0, times):
+            state = state.spin_cycle()
+        return state
+
+    def spin_cycles_with_cache(self, times):
+        states = [self]
+        state_by_str = {print_platform_state(self): 0}
+        state = self
+        repeats_at = 0
+        for i in range(0, times):
+            state = state.spin_cycle()
+            state_str = print_platform_state(state)
+            if state_str in state_by_str:
+                repeats_at = state_by_str[state_str]
+                break
+            else:
+                state_by_str[state_str] = i + 1
+                states.append(state)
+        repeating_len = len(states) - repeats_at
+        remainder = (times - repeats_at) % repeating_len
+        matching_index = repeats_at + remainder
+        return states[matching_index]
+
+    def get_spin_cycle_repeat(self):
+        state_by_str = {print_platform_state(self): 0}
+        state = self
+        i = 0
+        while True:
+            state = state.spin_cycle()
+            state_str = print_platform_state(state)
+            i += 1
+            if state_str in state_by_str:
+                previous_i = state_by_str[state_str]
+                return previous_i, i
+            else:
+                state_by_str[state_str] = i
+
     def total_load_on_north(self):
-        height = len(self.lines)
         total = 0
         for y, line in enumerate(self.lines):
             for tile in line:
                 if tile == 'O':
-                    total += height - y
+                    total += self.height - y
         return total
 
 
@@ -93,4 +132,11 @@ def print_platform_state(state):
     out = io.StringIO()
     for line in state.lines:
         print(''.join(line), file=out)
+    return out.getvalue()
+
+
+def print_platform_states(states):
+    out = io.StringIO()
+    for state in states:
+        print(print_platform_state(state), file=out)
     return out.getvalue()
--- day14/test_rocks.py
+++ day14/test_rocks.py
@@ -22,3 +22,43 @@ class TestRocks(unittest.TestCase):
         state = load_platform_state_from_file('example')
         spun = state.spin_cycle()
         verify(print_platform_state(spun))
+
+    def test_should_spin_cycle_twice_for_example(self):
+        state = load_platform_state_from_file('example')
+        spun = state.spin_cycles(2)
+        verify(print_platform_state(spun))
+
+    def test_should_spin_cycle_thrice_for_example(self):
+        state = load_platform_state_from_file('example')
+        spun = state.spin_cycles(3)
+        verify(print_platform_state(spun))
+
+    def test_should_spin_cycle_six_times_for_example(self):
+        state = load_platform_state_from_file('example')
+        spun = state.spin_cycles(6)
+        verify(print_platform_state(spun))
+
+    def test_should_spin_cycle_ten_times_for_example(self):
+        state = load_platform_state_from_file('example')
+        spun = state.spin_cycles(10)
+        verify(print_platform_state(spun))
+
+    def test_should_get_spin_cycle_repetition_for_example(self):
+        state = load_platform_state_from_file('example')
+        self.assertEqual((3, 10), state.get_spin_cycle_repeat())
+
+    def test_example_spin_cycles_repeat_between_3_and_10(self):
+        state = load_platform_state_from_file('example')
+        self.assertEqual(
+            print_platform_state(state.spin_cycles(3)),
+            print_platform_state(state.spin_cycles(10)))
+
+    def test_should_find_load_on_north_after_spins_for_example(self):
+        state = load_platform_state_from_file('example')
+        spun = state.spin_cycles_with_cache(1000000000)
+        self.assertEqual(64, spun.total_load_on_north())
+
+    def test_should_find_load_on_north_after_spins_for_input(self):
+        state = load_platform_state_from_file('input')
+        spun = state.spin_cycles_with_cache(1000000000)
+        self.assertEqual(90176, spun.total_load_on_north())
