4dc8944176eab1122479ca5d384f3df1da2890b3
mikehawes <152744602+mikehawes@users.noreply.github.com>
2023-12-21 14:10:10 +0000

    Day 21 part 1

--- day21/test_farm.py
+++ day21/test_farm.py
@@ -8,3 +8,7 @@ class TestFarm(unittest.TestCase):
     def test_should_count_tiles_reachable_for_example(self):
         farm = read_farm_map_from_file('example')
         self.assertEqual(16, farm.count_tiles_reachable(steps=6))
+
+    def test_should_count_tiles_reachable_for_input(self):
+        farm = read_farm_map_from_file('input')
+        self.assertEqual(3651, farm.count_tiles_reachable(steps=64))
