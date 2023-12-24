3d0f7a46f8530b917e15a7dc62d0ccc665ec4db3
mikehawes <152744602+mikehawes@users.noreply.github.com>
2023-12-24 19:03:21 +0000

    Day 24 part 1

--- day24/test_hail.py
+++ day24/test_hail.py
@@ -9,10 +9,14 @@ from day24.hail import load_hailstones_from_file, Position
 
 class TestHail(unittest.TestCase):
 
-    def test_should_count_2d_path_intersections_for_example(self):
-        hailstones = load_hailstones_from_file('example')
-        self.assertEqual(2, count_2d_intersections_in_area(hailstones, Position.all(7), Position.all(27)))
-
     def test_should_list_2d_path_intersections_for_example(self):
         hailstones = load_hailstones_from_file('example')
         verify(print_2d_intersections(hailstones))
+
+    def test_should_count_2d_path_intersections_for_example(self):
+        self.assertEqual(2, count_2d_intersections_in_area(
+            load_hailstones_from_file('example'), Position.all(7), Position.all(27)))
+
+    def test_should_count_2d_path_intersections_for_input(self):
+        self.assertEqual(18098, count_2d_intersections_in_area(
+            load_hailstones_from_file('input'), Position.all(200000000000000), Position.all(400000000000000)))
